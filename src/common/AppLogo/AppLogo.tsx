2b325cdad63b6e24ef459145c483aecbb693b571
nikitabelous <nikitabelous16@gmail.com>
2023-12-11 18:48:31 +0200

    fix logo redirect

--- src/common/AppLogo/AppLogo.tsx
+++ src/common/AppLogo/AppLogo.tsx
@@ -13,7 +13,7 @@ const AppLogo: FC<HTMLAttributes<HTMLDivElement>> = ({
   return (
     <div className={`app-logo ${className}`} {...rest}>
       <img src='/branding/logo.svg' alt={config.APP_NAME} />
-      <Link className='app-logo__link' to={RoutesPaths.App} />
+      <Link className='app-logo__link' to={RoutesPaths.Profiles} />
     </div>
   )
 }
